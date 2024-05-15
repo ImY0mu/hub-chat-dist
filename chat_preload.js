0f3509fbdaec47a94b0f54098b3abfc4540793ac
ImY0mu <justy0mu@gmail.com>
2024-05-15 23:38:36 +0200

    0.0.36

--- chat_preload.js
+++ chat_preload.js
@@ -1,58 +1,60 @@
 const { ipcRenderer } = require("electron");
-//const { post } = require("got");
 
 global.sendToWindow = (type, args = undefined) => {
-  if(args == undefined) return ipcRenderer.sendToHost(type)
-  ipcRenderer.sendToHost(type, args)
+    if (args == undefined) return ipcRenderer.sendToHost(type)
+    ipcRenderer.sendToHost(type, args)
 }
+
 var isReady = true;
 
 document.addEventListener('DOMContentLoaded', (event) => {
-  if(isReady == false) return console.log("Page was already loaded once before.");
-  isReady = false;
-  console.log("Page loaded.");
+    if (isReady == false) return console.log("Page was already loaded once before.");
+    isReady = false;
+    console.log("Page loaded.");
 
-  getRequiredScripts(window.location.href.toString())
-  .then(data = (data) => {
-    var script = document.createElement('script'); 
-    script.className = "SimScript";
-    script.innerHTML = data;
-    script.onload = function() {
-      this.remove();
-    };
-    (document.head || document.documentElement).appendChild(script);
-  })
-  .catch(error => console.log(error));
+    getRequiredScripts(window.location.href.toString())
+        .then(data = (data) => {
+            var script = document.createElement('script');
+            script.className = "SimScript";
+            script.innerHTML = data;
+            script.onload = function () {
+                this.remove();
+            };
+            (document.head || document.documentElement).appendChild(script);
+        })
+        .catch(error => console.log(error));
 
-  window.addEventListener("message", function(event) {
-    //console.log(event);
-    sendToWindow(event.data.type, event.data.data);
-  });
-  //END OF LOAD
+    window.addEventListener("message", function (event) {
+        sendToWindow(event.data.type, event.data.data);
+    });
 })
 
 
 window.addEventListener('load', function () {
-  getRequiredScriptsAfter(window.location.href.toString())
-  .then(data = (data) => {
-    var script = document.createElement('script'); 
-    script.className = "SimEndScript";
-    script.innerHTML = data;
-    script.onload = function() {
-      this.remove();
-    };
-    (document.body).insertAdjacentElement('beforeend', script);
-  })
-  .catch(error => console.log(error));
+    getRequiredScriptsAfter(window.location.href.toString())
+        .then(data = (data) => {
+            var script = document.createElement('script');
+            script.className = "SimEndScript";
+            script.innerHTML = data;
+            script.onload = function () {
+                this.remove();
+            };
+            (document.body).insertAdjacentElement('beforeend', script);
+        })
+        .catch(error => console.log(error));
 })
 
 
 
 
 const getRequiredScripts = async (url) => {
-  var script = "";
-  if(url.includes('https://simple-mmo.com/chat/public?')){
-    script += `
+    var script = "";
+    if (url.includes('https://simple-mmo.com/chat/public?')) {
+        script += `
+
+    /**
+     * Send the data to chat client.
+     */
     function sendData(type, data){
       if(data == undefined || data == null) return console.warn('No data');
       var item = {
@@ -62,95 +64,177 @@ const getRequiredScripts = async (url) => {
       window.postMessage(item);
     }
 
-    function setupListeners(){
-      console.log('SET UP');
-      var item = document.querySelectorAll('[x-show="rewards.item"]')[1];
-      var diamonds = document.querySelectorAll('[x-show="rewards.diamonds"]')[1];
-      var exp = document.querySelectorAll('[x-show="rewards.exp"]')[1];
+    /**
+     * Setup listeners for chatting rewards/drops.
+     */
+    function setupListeners() {
+        var item = document.querySelectorAll('[x-show="rewards.item"]')[1];
+        var diamonds = document.querySelectorAll('[x-show="rewards.diamonds"]')[1];
+        var exp = document.querySelectorAll('[x-show="rewards.exp"]')[1];
 
-
-      const observer = [
-        new MutationObserver((mutations, observer) => {
-          var value = mutations[0].target.innerHTML;
-          if(value != 0 && value != null && value != '' && value != '0'){
-            console.log('You have earned item: ' + value);
-            sendData('chat_drop', { type: 'item', value: value });
-          }
-        }),
-        new MutationObserver((mutations, observer) => {
-          var value = mutations[0].target.innerHTML;
-          if(value != 0 && value != null && value != '' && value != '0'){
-            console.log('You have earned diamonds: ' + value);
-            sendData('chat_drop', { type: 'diamonds', value: value });
-          }
-        }),
-        new MutationObserver((mutations, observer) => {
-          var value = mutations[0].target.innerHTML;
-          if(value != 0 && value != null && value != '' && value != '0'){
-            console.log('You have earned exp: ' + value);
-            sendData('chat_drop', { type: 'exp', value: value });
-          }
-        })
-      ]
-
-      observer[0].observe(item, {
-        childList: true
-      });
-
-      observer[1].observe(diamonds, {
-        childList: true
-      });
-
-      observer[2].observe(exp, {
-        childList: true
-      });
+        
+        var exp = document.querySelectorAll('[x-show="rewards.exp"]')[1];
+    
+        const observer = [
+            new MutationObserver((mutations, observer) => {
+                var value = mutations[0].target.innerHTML;
+    
+                if (value != 0 && value != null && value != "" && value != "0")
+                    sendData("chat_drop", { type: "item", value: value });
+            }),
+            new MutationObserver((mutations, observer) => {
+                var value = mutations[0].target.innerHTML;
+    
+                if (value != 0 && value != null && value != "" && value != "0")
+                    sendData("chat_drop", { type: "diamonds", value: value });
+            }),
+            new MutationObserver((mutations, observer) => {
+                var value = mutations[0].target.innerHTML;
+                
+                if (value != 0 && value != null && value != "" && value != "0")
+                    sendData("chat_drop", { type: "exp", value: value });
+            }),
+        ];
+    
+        observer[0].observe(item, {
+            childList: true,
+        });
+    
+        observer[1].observe(diamonds, {
+            childList: true,
+        });
+    
+        observer[2].observe(exp, {
+            childList: true,
+        });
     }
 
+    /**
+     * No idea what this does so not used.
+     */
+    function autoRefresh(){
+        setTimeout(() => {
+            setTimeout(() => {
+                window.location.reload();
+            }, 1000);
+        }, 1000 * 60 * 5);
+    }
+
+    /**
+     * Disable the automatic chat disconnect.
+     */
+    function disableDisconnect(){
+        setTimeout(() => {
+            window.Echo.connector.pusher.connection.disconnect = () => {
+                console.log("Disconnect is disabled.");
+            }
+        }, 10_000);
+        
+    }
+
+   
+    
+    /**
+     * Initialise the chat messages listener.
+     */
+    function initialiseChat() {
+        window.Alpine.effect(() => {
+            getChatMessages();
+            getBlockedPlayers();
+            getItemFromPopup();
+        })
+    }
+
+
+    
+    let last_chat_id = null;
+
+    /**
+     * Get the chat messages.
+    */ 
+    function getChatMessages(){
+        let chats = Alpine.store('chats');
+
+        if(chats[0].id === last_chat_id)
+            return;
+            
+        sendData('chatUpdate', JSON.stringify(chats));
+
+        last_chat_id = chats[0].id;
+    }
+
+    /**
+    * Get the blocked players.
+    */
+    function getBlockedPlayers(){
+        try {
+            sendData('blocked_players', JSON.stringify(document.getElementById('smmo-chat')._x_dataStack[0].blocked_players));
+        } catch (error) {
+            console.log('Could not send the blocked players list as it does not exist yet.');
+        }
+    }
+
+    /**
+     * Overwrite the function for retrieving items to show them in chat client.
+     */
+    function getItemFromPopup(){
+        let item = document.getElementById('item-popup')._x_dataStack[0].item;
+
+        if(item.id === null)
+            return;
+        
+        var item_data = {
+            id: item.id,
+            name: btoa(item.name),
+            stats: item.stats,
+            image: item.image,
+            type: item.type,
+            description: item.description,
+            equipable: item.equipable,
+            currently_equipped: item.currently_equipped,
+            value: item.value,
+            rarity: item.rarity,
+            level: item.level,
+            circulation: item.circulation,
+            yours: item.yours,
+            market: item.market,
+            current_stats: item.current_stats,
+            item_collection: item.item_collection,
+            collectable_collection: item.collectable_collection,
+            sprite_collection: item.sprite_collection,
+            background_collection: item.background_collection,
+            avatar_collection: item.avatar_collection,
+            custom_item: item.custom_item,
+            additional_data: item.additional_data,
+        };
+
+        resetData();
+
+        sendData('showItem', JSON.stringify(item_data));
+    }
+
+    
     setupListeners();
-
-    sendData('blocked_players', JSON.stringify(document.getElementById('smmo-chat')._x_dataStack[0].blocked_players));
+    //autoRefresh();
+    initialiseChat();
+    disableDisconnect();
+    overwriteRetrieveItem();
     
-    sendData('chatChannel', active_channel);
 
-    eval(listenForMessages.toString().replace("//Correct chat length if too long", "sendData('chatUpdate', JSON.stringify(Alpine.store('chats')));"));
-
-    eval(updateChatWindow.toString().replace("Alpine.store('loading_icon', true);", "Alpine.store('loading_icon', true); console.log(active_channel); sendData('chatUpdate', JSON.stringify(Alpine.store('chats')));"));
-    
-    
-    
-    eval(retrieveFromServer.toString().replace("Alpine.store('chats', data);", "Alpine.store('chats', data); sendData('chatUpdate', JSON.stringify(data));"));
-
-    eval(disconnectFromChat.toString().replace("window", "//window"));
-
-    eval(retrieveItem.toString().replace(".then(function(data){", ".then(function(data){ sendData('showItem', JSON.stringify(data));"));
-
-
-    
-    setTimeout(() => {
-      console.log('Reloading');
-      setTimeout(() => {
-        window.location.reload();
-      }, 1000);
-    }, 1000 * 60 * 5);
   `;
-  }
-  return script;
+    }
+    return script;
 }
 
 
 
 
 const getRequiredScriptsAfter = async (url) => {
-  var script = "";
-  if(url.includes('https://simple-mmo.com/chat/public?')){
-    script += `
+    var script = "";
+    if (url.includes('https://simple-mmo.com/chat/public?')) {
+        script += `
     //eval(changeChannel.toString().replace("updateChatWindow(); fixChat();", "sendData('chatUpdate', JSON.stringify(Alpine.store('chats')));"));
   `;
-  }
-  return script;
+    }
+    return script;
 }
-
-
-
-/* Additional functions */
-
